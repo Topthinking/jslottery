860bdcf0fe584db01bc22365b1cbd04243f3ac36
Topthinking <1991syj@sina.cn>
2015-10-27 22:01:11 +0800

    0.01

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# Jslottery
-Js抽奖库
+JS抽奖类库(依赖于juqery库)
+@author Topthinking
+
+1.相关的参数都有说明
+2.调用(下面显示的都是必填项)
+new Jslottery({
+	scroll_dom:'', //用来滚动的dom元素(必填)
+	id:0, //最终在哪个元素处停止(必填)
+	scroll_value:'',//滚动元素的属性值(必填)
+	original_value:'',//元素原来的属性值(必填)
+	attr_id:'',//滚动元素上绑定的奖品号的属性名称,(必填)
+	change_mode:'',//元素属性名称(必填)
+	callback:function(data){
+		//这里进行回调函数的处理
+		switch(data.status){
+				case '0': //点击抽奖接口
+					
+				break;
+
+				case '1': //抽奖程序结束
+					console.log(data.data);
+				break;
+
+				default:
+				break;
+			}
+	} //回调函数(必填)
+});
---
+++ jslottery.js
@@ -0,0 +1,169 @@
+/**
+ * js抽奖类库
+ * @author Topthinking
+ */
+;(function(window, factory){
+if(typeof define == 'function'){
+  define(function(require, exports, module){
+    return factory(require('common:jquery'));
+  });
+}else{
+  window.FeatherUi = window.FeatherUi || {};
+  window.FeatherUi.Template = factory();
+}
+})(window,function($){
+
+//global 对象全局this
+//curL  当前滚动元素的位置(标志位)
+//curC  当前转的总圈数
+//num  每一圈的总元素个数
+//steps 每次点击抽奖实时转动的步数
+var global,curL=1,curC=0,num,steps=0;
+
+function Jslottery(opt){
+	this.options = $.extend({
+		scroll_dom:'', //用来滚动的dom元素(必填)
+		id:0, //最终在哪个元素处停止(必填)
+		speed:300,//转速(默认)
+		speedUp:50,//加速度(默认)
+		speedDown:400,//减速度(默认)
+		speed_up:3,//加速处,相对于当前位置加(默认)
+		speed_down:2,//减速处,相对于结束位置相减(默认)
+		countC:1,//需要转的圈数(默认)
+		scroll_value:'',//滚动元素的属性值(必填)例如背景颜色
+		original_value:'',//元素原来的属性值(必填)例如背景颜色
+		attr_id:'',//滚动元素上绑定的奖品号的属性名称，例如：data-id="1"(必填)
+		change_mode:'',//元素属性名称(必填)例如是改变背景颜色还是边框啊
+		callback:function(){} //回调函数(必填)
+	},opt || {});
+
+	//固定不变的元素
+	this.fixs = {
+		cur_speed_up:'',//当前加速位置
+		cur_speed_down:'',//当前的减速位置
+		timeout:false,//用来关闭定时器的标识位
+		original_speed:''//初始传进来的速度
+	};
+
+	this.init();
+}
+
+Jslottery.prototype = {
+	init:function(){
+		global = this;
+		var self = global.options;
+		var selfs = global.fixs;
+		num = $(self.scroll_dom).length;	
+		selfs.original_speed = self.speed;
+	},
+
+	start:function(){
+		var self = global.options;
+		var selfs = global.fixs;
+		var callback={'status':'0'}
+		self.callback(callback);
+		if(selfs.timeout)//这里是转盘结束
+		{   
+			curC=0;
+			steps=0;
+			global.options.speed = selfs.original_speed;
+			selfs.timeout = false;			
+			global.stop();
+			clearTimeout(global.start);
+			return false;
+		}
+		global.changeNext();
+		setTimeout(global.start,self.speed);
+	},
+
+	//转盘停下后回调函数
+	stop:function(){
+		var self = global.options;
+		var callback={'status':'1','data':global}
+		global.options.callback(callback);
+	},
+
+	speedUp:function(){
+		if(steps==global.options.speed_up)
+		{
+			global.options.speed = global.options.speedUp;
+		}
+	},
+
+	speedDown:function(){
+		var tmp1 = global.options.id-global.options.speed_down;
+		var tmp2 = global.options.countC+1;
+		if(tmp1<=0)
+		{
+			tmp1 = num + tmp1;
+			tmp2 = tmp2-1;
+		}
+
+		if(curL==tmp1 && curC==tmp2)
+		{
+			global.options.speed = global.options.speedDown;
+		}
+	},
+
+	changeNext:function(){
+
+		var self = global.options;
+		var selfs = global.fixs;
+		steps++;
+		
+		if(curL==num+1)
+		{
+			curL=1;
+			curC++;
+		}
+
+		//加速
+		global.speedUp();
+
+		//减速
+		global.speedDown();
+
+		if(curL==self.id && curC==self.countC+1) //这里表示转盘结束
+		{
+			selfs.timeout = true;
+		}
+
+		global.start_scroll();
+	},
+
+	start_scroll:function(){
+		var self = global.options;
+		var scroll_json = {};
+		var original_json = {};
+
+		scroll_json[self.change_mode] = self.scroll_value;
+		original_json[self.change_mode] = self.original_value;
+		//遍历需要滚动的元素
+		$(self.scroll_dom).each(function(){
+			//根据每个元素的标识号来改变颜色			
+			if($(this).attr(self.attr_id) == curL)
+			{					
+				$(this).css(scroll_json);				
+				$(self.scroll_dom).each(function(){
+					if(curL==1)
+					{
+						$(self.scroll_dom).each(function(){
+							if($(this).attr(self.attr_id) == num)
+							{
+								$(this).css(original_json);
+							}
+						});
+					}
+					else if($(this).attr(self.attr_id) == curL-1)
+					{
+						$(this).css(original_json);
+					}
+				});
+				curL++;
+				return false;
+			}
+		});
+	}
+};
+return Jslottery;
+});
